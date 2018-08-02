c56415b5717085faec81e2194cd611c8f9f1f9e6
Viktor <Ponomarenkovic1979@gmail.com>
2018-08-02 23:50:01 +0300

    Add project files.

---
+++ SoftServeTrialTest/Animal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftServeTrialTest
+{
+    public abstract class Animal
+    {
+        protected Animal(string name, string breed)
+        {
+            Name = name;
+            Breed = breed;
+        }
+        public string Name { get; set; }
+        public string Breed { get; set; }
+
+        public virtual void Introduce()
+        {
+            Console.WriteLine("Name - {0}, breed - {1}", Name, Breed);
+        }
+    }
+}
---
+++ SoftServeTrialTest/PetShop.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftServeTrialTest
+{
+    public class PetShop : IEnumerable<Animal>
+    {
+        public PetShop()
+        {
+            _animal = new List<Animal>();
+        }
+
+        public void Add(Animal animal)
+        {
+            _animal.Add(animal);
+        }
+
+        public IEnumerator<Animal> GetEnumerator()
+        {
+           return _animal.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void IntroduceAll()
+        {
+            foreach (var animal in _animal)
+            {
+                animal.Introduce();
+            }
+        }
+
+        private readonly List<Animal> _animal;
+    }
+}
